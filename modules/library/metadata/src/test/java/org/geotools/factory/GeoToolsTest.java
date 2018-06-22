81dcbc29a3b3b74dd80731ec4c2e8935b87f66bb
Jim Hughes <jnh5y@ccri.com>
2018-06-22 19:39:08 -0400

    JTS Version update in a test.
    Signed-off-by: Jim Hughes <jnh5y@ccri.com>

--- modules/library/metadata/src/test/java/org/geotools/factory/GeoToolsTest.java
+++ modules/library/metadata/src/test/java/org/geotools/factory/GeoToolsTest.java
@@ -162,7 +162,7 @@ public final class GeoToolsTest {
 
         version = GeoTools.getVersion(Geometry.class);
         assertNotNull(version);
-        assertEquals("1.14.0", version.toString());
+        assertEquals("1.15.0", version.toString());
     }
     /** Tests the use of system properties. */
     @Test
